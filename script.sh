d107b7153353f4251d31a0d2a24aa1b02ab3bff4
Jun-Hao Ryan <943660539@qq.com>
2025-12-24 19:19:45 +0800

    New Update from 24/12/2025

--- script.sh
+++ script.sh
@@ -3,3 +3,11 @@ git rm --cached *.pt
 
 git branch -d branch_name
 git branch -D branch_name
+
+
+git add .gitignore
+git commit -m "Update .gitignore"
+
+git rm -r --cached .
+git add -A
+git commit -m "Apply .gitignore and stop tracking ignored files"
